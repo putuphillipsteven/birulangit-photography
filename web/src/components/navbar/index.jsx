a31f8a804a5e009487943fe89da1ebd6b9182e5f
phillip-steven <putu.phillip@gmail.com>
2024-02-19 21:30:02 +0700

    feat(tailwind): tailwind initialization

---
+++ web/src/components/navbar/index.jsx
@@ -0,0 +1,8 @@
+export const Navbar = () => {
+	return (
+		<div>
+			<h1 className="text-3xl">Hello world!</h1>
+			<button class="bg-indigo-500 ...">Save changes</button>
+		</div>
+	);
+};
---
+++ web/src/pages/landing-page/index.jsx
@@ -0,0 +1,10 @@
+import { Navbar } from "../../components/navbar";
+
+export const LandingPage = () => {
+	return (
+		<div>
+			<Navbar />
+			<p className="font-bold">Choose your packages</p>
+		</div>
+	);
+};
